6e610d275d8dcdc1377fdade520d917cbd6ba0dc
Lee Juneyoung <wnsdud318@gmail.com>
2023-06-17 01:48:25 +0900

    🐛Fix: 전송요청 후 체크가 아니라 체크 후 전송하도록 바꿈.

--- static/js/article.js
+++ static/js/article.js
@@ -1,16 +1,15 @@
 window.onload = () => {
-    // 게시글 가져오기
-    loadArticles();
-
     showName();
-
 }
 
 /**
  * 작성자 : 공민영
  * 내용 : 게시글 작성하기
  * 최초 작성일 : 2023.06.15
- * 업데이트 일자 : 2023.06.15
+ * 최종 수정자 : 이준영
+ * 수정내용 : 작성완료 시 오류 나면 초기화하는 문제
+ * => 전송요청 후 체크가 아니라 체크 후 전송하도록 바꿈.
+ * 업데이트 일자 : 2023.06.17
  */
 async function postArticle() {
     console.log("게시글 작성 눌림");
@@ -28,19 +27,17 @@ async function postArticle() {
 
     let access_token = localStorage.getItem("access_token");
 
-    const response = await fetch(`${backend_base_url}/article/`, {
-        method: 'POST',
-        headers: {
-            "Authorization": `Bearer ${access_token}`
-        },
-        body: formdata
-    })
     if (title == "" || content == "" || song == null) {
-        alert("제목,내용,음악파일은 필수");
-        location.reload();
-    } else if (article_image == null) {
-        formdata.append('article_image', '');
+        alert("제목, 내용, 음악파일은 필수입니다.");
+        return;
     } else {
+        const response = await fetch(`${backend_base_url}/article/`, {
+            method: 'POST',
+            headers: {
+                "Authorization": `Bearer ${access_token}`
+            },
+            body: formdata
+        })
         if (response.status == 201) {
             alert("작성완료!");
             window.location.replace('index.html');
@@ -77,81 +74,6 @@ async function getArticles() {
 function articleDetail(articleId) {
     console.log(articleId);
     window.location.href = `${frontend_base_url}/article_detail.html?article_id=${articleId}`
-}
-
-/**
- * 작성자 : 공민영
- * 내용 : index에 게시글 불러오기
- * 최초 작성일 : 2023.06.15
- * 업데이트 일자 : 2023.06.15
- */
-console.log('리스트 연결');
-
-async function loadArticles() {
-
-    articles = await getArticles();
-    console.log(articles);
-
-    const article_list = document.getElementById("article_list");
-
-    articles.forEach(article => {
-        const newCol = document.createElement("div");
-        newCol.setAttribute("class", "col");
-        newCol.setAttribute("onclick", `articleDetail(${article.id})`);
-
-        const newCard = document.createElement("div");
-        newCard.setAttribute("class", "card");
-        //클릭했을때 id값으로 이동하기 위함(각 card당 id넣어줌)
-        newCard.setAttribute("id", article.id);
-
-        newCol.appendChild(newCard);
-
-        //제목이 들어있는 바디
-        const newCardBody = document.createElement("div");
-        newCardBody.setAttribute("class", "card_body");
-        newCard.appendChild(newCardBody);
-
-        //제목
-        const newCardTitle = document.createElement("h4");
-        newCardTitle.setAttribute("class", "card_title");
-        newCardTitle.innerText = article.title;
-        newCardBody.appendChild(newCardTitle);
-
-        //이미지와 음악이 들어있는 바디
-        const newCardPlay = document.createElement("div");
-        newCardPlay.setAttribute("class", "card_play");
-        newCard.appendChild(newCardPlay);
-
-        //이미지
-        const articleImage = document.createElement("img");
-        articleImage.setAttribute("class", "card_img_top");
-        if (article.article_image) { //이미지 있으면
-            articleImage.setAttribute("src", `${backend_base_url}${article.article_image}/`)
-        } else { //이미지 없으면
-            articleImage.setAttribute("src", "default.PNG");
-        }
-        newCardPlay.appendChild(articleImage);
-
-        //음악
-        const articleSong = document.createElement("audio")
-        articleSong.setAttribute("class", "card_file")
-        articleSong.setAttribute("src", `${backend_base_url}${article.song}`)
-        newCardPlay.appendChild(articleSong)
-
-        // Audio 객체 설정
-        // const myAudio = new Audio();
-        // myAudio.src = `${backend_base_url}${article.article_image}`;
-
-        // // 오디오 재생: 크롬브라우저에서 작동 안함
-        // myAudio.play();
-
-        // const btnPlay = document.getElementById("btnPlay");
-        // const btnPause = document.getElementById("btnPause");
-        // const btnStop = document.getElementById("btnStop");
-
-        article_list.appendChild(newCol);
-
-    });
 }
 
 //작성 이미지 미리보기
