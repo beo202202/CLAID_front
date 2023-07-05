c5beb03d6818e65b420c7b6739d7f6a5eb3461fd
madonghwi <ehd0831@naver.com>
2023-07-05 11:54:34 +0900

    feat: 프로필 기능(미완)

--- static/js/profile.js
+++ static/js/profile.js
@@ -1,20 +1,67 @@
-/**
- * 작성자 : 마동휘
- * 내용 : 프로필 정보 가져옴
- * 최초 작성일 : 2023.06.18
- * 업데이트 일자 : 2023.06.19
- */
+async function fetchUserProfileData() {
+    const userId = getUserId();
+    if (!userId) {
+      console.error('사용자 ID를 가져오지 못했습니다.');
+      return;
+    }
+  
+    const accessToken = getAccessToken();
+    if (!accessToken) {
+      console.error('액세스 토큰이 없습니다.');
+      return;
+    }
+    $.ajax({
+      url: `${backend_base_url}/user/profile/${userId}/`,  // 프로필 API 엔드포인트의 URL을 여기에 입력
+      method: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      success: function(response) {
 
-window.addEventListener("DOMContentLoaded", () => {
-  loadProfile();
-});
-
-async function loadProfile() {
-  const payload = localStorage.getItem("payload");
-  const payload_parse = JSON.parse(payload);
-  const nickname = document.getElementById("user-nickname");
-  const profile_image = document.getElementById("image_profile");
-
-  nickname.innerText = `${payload_parse.nickname}`;
-  profile_image.src = payload_parse.profile_image;
-}
+        var profileData = response;  // API 응답 데이터
+        // 가져온 데이터를 HTML 요소에 업데이트
+        $('#image_profile').attr("src", profileData.profile_image);
+        $('#user-nickname').text(profileData.nickname);
+      },
+      error: function(error) {
+        // 에러 발생 시의 처리 로직
+        console.log(error);
+      }
+    });
+    $.ajax({
+      url: `${backend_base_url}/article/?user_id=${userId}`,
+      type: 'GET',
+      headers: {
+        Authorization: `Bearer ${accessToken}`,
+      },
+      success: function (data) {
+        // 게시글을 표시할 컨테이너 선택
+        var articlesContainer = $('#articles-container');
+        articlesContainer.empty(); // 기존 게시글 초기화
+  
+        // 각 게시글을 반복하여 표시
+        for (var i = 0; i < data.length; i++) {
+          var article = data[i];
+          var articleElement = $('<div class="article">');
+          articleElement.text(article.song_info);
+  
+          articleElement.click((function (article) {
+            return function () {
+              var articleUrl = "article_detail.html?article_id=" + article.id;
+              window.location.href = articleUrl;
+            };
+          })(article));
+  
+          articlesContainer.append(articleElement);
+        }
+      },
+      error: function () {
+        // 에러 처리
+      }
+    });
+  }
+  
+  $(document).ready(function () {
+    fetchUserProfileData();
+  });
+  
